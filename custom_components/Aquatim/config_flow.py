a447a49a5235153b43dd272ee8875ae160cfa6e8
v1n3ri <v1n3ri@yahoo.com>
2026-03-29 15:51:34 +0300

    fix

--- custom_components/Aquatim/config_flow.py
+++ custom_components/Aquatim/config_flow.py
@@ -1,14 +1,18 @@
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_EMAIL, CONF_PASSWORD
 import voluptuous as vol
+from .const import DOMAIN, CONF_EMAIL, CONF_PASSWORD
 
 class AquatimConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Aquatim."""
-    VERSION = 1  # <--- Asigură-te că această linie există
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Pasul initial de configurare."""
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_EMAIL], data=user_input)
+            return self.async_create_entry(
+                title=user_input[CONF_EMAIL], 
+                data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
